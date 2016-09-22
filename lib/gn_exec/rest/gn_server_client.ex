9cdb1883191cb11cc20769c5660ed53094dc1e0e
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2016-09-22 07:55:07 +0200

    let process the body of a response by its caller

--- lib/gn_exec/rest/gn_server_client.ex
+++ lib/gn_exec/rest/gn_server_client.ex
@@ -12,7 +12,7 @@ defmodule GnExec.Rest.Client do
 
   def process_response_body(body) do
     body
-    |> Poison.decode!(as: %GnExec.Rest.Job{})
+    #|> Poison.decode!(as: %GnExec.Rest.Job{})
     # |> Map.take(@expected_fields)
 
     #|> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
@@ -20,5 +20,19 @@ defmodule GnExec.Rest.Client do
 
   def get_a_job(program, parameter) do
     get!(program <> "/dataset.json").body
+    |> Poison.decode!(as: %GnExec.Rest.Job{})
+  end
+
+  def get_status(token) do
+    IO.puts "program/" <> token <> "/status.json"
+    get!("program/" <> token <> "/status.json").body
+    |> Poison.decode!(as: %GnExec.Rest.JobStatus{} )
+  end
+
+
+  def set_status(token, progress) do
+    url = Application.get_env(:gn_exec, :gn_server_url)
+    response = put!("program/" <> token <> "/status.json",{:form, [{:progress, progress}]})
+
   end
 end
